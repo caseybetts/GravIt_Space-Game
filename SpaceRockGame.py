d9954c7c8367b7e54a03f18b0307362947585482
caseybetts <casey.betts@colorado.edu>
2022-09-12 17:59:14 -0600

    Put the displacement calculations in a separate function. Takes a sprite group and a spacerock object and returns a tuple with x,y displacment values.

--- SpaceRockGame.py
+++ SpaceRockGame.py
@@ -11,82 +11,121 @@ G = 6.6743e-11 # m3 kg-1 s-2
 framerate = 10
 winHeight = 400
 winWidth = 800
+number_of_rocks = 3
 
-class SpaceRock():
+pygame.init()
+
+
+# Create a class for the space rocks extending pygame sprite class
+class SpaceRock(pygame.sprite.Sprite):
     def __init__(self,m,p,v):
+        super(SpaceRock,self).__init__()
 
         # Mass, Position and Velocity parameters initialized
         self.mass = m
         self.position = p
         self.velocity = v
 
-    def move(self,space_objects):
-        # Updates the object's position
-        force_x = 0
-        force_y = 0
+        if pygame.get_init():
+            self.surface = pygame.Surface((10,10))
+            self.surface.fill("Red")
+            self.rect = self.surface.get_rect()
+        else:
+            print("pygame is not initialized")
 
-        for obj in space_objects:
-            # Check if objects are too close
-            if abs(self.position[0] - obj.position[0]) < 1 and abs(self.position[1] - obj.position[1]) < 1:
-                continue
-            # Calculate force in x direction
-                # by finding the distance in the x direction
-            r_x = self.position[0] - obj.position[0]
-            f_x = (G*self.mass*obj.mass)/((r_x)**2) # N (force)
-            # Calculate force in y direction
-                # by finding the distance in the x direction
-            r_y = self.position[1] - obj.position[1]
-            f_y = (G*self.mass*obj.mass)/((r_y)**2) # N (force)
-            # Add object force to total force
-            force_x += f_x
-            force_y += f_y
-        # Calculate displacement in X
-        dist_x = (((1/framerate)**2)*(force_x/self.mass)*(.5)) + self.velocity[0]*(1/framerate)
-        # Calculate displacement in y
-        dist_y = (((1/framerate)**2)*(force_y/self.mass)*(.5)) + self.velocity[1]*(1/framerate)
+    def update(self):
+        # Find the displacement in position
+        dist = displacement(rocks, self)
         # Update position
-        self.position[0] += dist_x
-        self.position[1] += dist_y
+        self.rect.move_ip(dist[0],dist[1])
 
+# This will take in a group of rocks and determin the displacement of a given rock
+def displacement(group,target):
+
+    total_force_x = 0
+    total_force_y = 0
+    # For each rock in the sprite group calculate the force on the target rock
+    for rock in group:
+        if (target.position[0] - rock.position[0]) < 1 and (target.position[1] - rock.position[1]) < 1:
+            continue
+        # Calculate force in x direction by finding the distance in the x direction
+        distance_x = target.position[0] - rock.position[0]
+        force_x = (G*target.mass*rock.mass)/((distance_x)**2) # N (force)
+        # Calculate force in x direction by finding the distance in the y direction
+        distance_y = target.position[1] - rock.position[1]
+        force_y = (G*target.mass*rock.mass)/((distance_y)**2) # N (force)
+        # Add object force to total force
+        total_force_x += force_x
+        total_force_y += force_y
+    # Calculate the displacement of the tartet rock
+    # Calculate displacement in X
+    displacement_x = (((1/framerate)**2)*(total_force_x/target.mass)*(.5)) + target.velocity[0]*(1/framerate)
+    # Calculate displacement in y
+    displacement_y = (((1/framerate)**2)*(total_force_y/target.mass)*(.5)) + target.velocity[1]*(1/framerate)
+
+    # Return displacement in a tuple
+    return (displacement_x, displacement_y)
+
+# Create a sprite group to contain the space rocks
+rocks = pygame.sprite.Group()
 
 class Game():
     # Contains the loop for running the game
-    def __init__(self, space_objects_input):
-        self.rocks = space_objects_input
-
-        # Initiallize pygame
-        pygame.init()
+    def __init__(self):
         # Create a display window
-        screen = pygame.display.set_mode((winWidth,winHeight))
+        self.screen = pygame.display.set_mode((winWidth,winHeight))
         # Create a clock to control frames per second
         self.clock = pygame.time.Clock()
 
     def run(self):
         # Contains the loop to render the game and exit on quit event
+        print("is pygame init? ", pygame.get_init())
+
 
         while True:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.cleanup()
+                elif event.type == pygame.QUIT:
                     self.cleanup()
-            j = 1
-            for i in self.rocks:
-                i.move(self.rocks)
-                print(f"rock {j} is at ({i.position[0]},{i.position[1]})")
-                j += 1
-            self.clock.tick(2)
+            # White background
+            self.screen.fill('White')
+
+            # Update rock positions
+            rocks.update()
+
+            # Draw all sprites
+            for entity in rocks:
+                self.screen.blit(entity.surface, entity.rect)
+
+            # for i in range(number_of_rocks):
+            #     print(f"rock {i}")
+            #     self.rocks[i].move(self.rocks)
+            #     print(int(self.rocks[i].position[0]), "and",int(self.rocks[i].position[1]) )
+                #self.screen.blit(self.rocks[i].surface,(int(self.rocks[i].position[0]),int(self.rocks[i].position[1])))
+
+            self.clock.tick(10)
+
+            pygame.display.flip()
 
     def cleanup(self):
-        print("cleanup")
         pygame.quit()
         exit()
+        print("cleanup complete")
 
-
-
+# returns a list of space rock objects
 def make_rocks(num):
     # Creating a host of space rocks of random size and posititon
     rocks = []
     for i in range(num):
-        rocks.append(SpaceRock(random.random()*10000000000000000, [100-random.random()*200,100-random.random()*200], [10-random.random()*20,10-random.random()*20] ))
+        rocks.append(
+            SpaceRock(
+                random.randint(1,10)*10000000000000000,
+                [random.randint(1,10)*winWidth,random.randint(1,10)*winHeight],
+                [10-random.randint(1,10)*20,10-random.randint(1,10)*20]
+                )
+            )
     # Printing out the rock's size and position
     # for i in range(num):
     #     print(f"The mass of space rock {i} is {rocks[i].mass}")
@@ -95,6 +134,10 @@ def make_rocks(num):
     return rocks
 
 if __name__ == "__main__":
-    my_rocks = make_rocks(3)
-    game1 = Game(my_rocks)
+    # Create the rocks and add them to a sprite group
+    rock_list = make_rocks(number_of_rocks)
+    for rock in rock_list:
+        rocks.add(rock)
+    # Create game object and run
+    game1 = Game()
     game1.run()
