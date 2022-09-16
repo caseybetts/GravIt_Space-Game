6895655eb15bb6bbd0a5ecb034291fc7ea201f27
caseybetts <casey.betts@colorado.edu>
2022-09-16 16:31:33 -0600

    Added a radar to the screen

--- SpaceRockGame.py
+++ SpaceRockGame.py
@@ -8,7 +8,7 @@ from pygame.locals import *
 from sys import exit
 import random
 import math
-from Calculations import find_force
+from Calculations import find_force, radar_coord_conversion
 
 # Import pygame.locals for easier access to key coordinates
 from pygame.locals import (
@@ -36,6 +36,19 @@ percent_ejection = .001
 rebound_velocity = 2
 collision_slow_percent = .99
 MASSES = (100000, 500000, 2000000)
+radar_reduction = .07
+
+# Outer Boudary
+outer_left = -winWidth
+outer_right = 2*winWidth
+outer_top = -winHeight
+outer_bottom = 2*winHeight
+map_width = outer_right-outer_left
+map_height = outer_bottom-outer_top
+
+# Radar coords
+radar_left = winWidth-20-(map_width*radar_reduction)
+radar_top = winHeight-20-(map_height*radar_reduction)
 
 pygame.init()
 
@@ -158,6 +171,50 @@ class Player(pygame.sprite.Sprite):
         if self.rect.bottom >= winHeight:
             self.velocity[1] = -1
 
+class RadarPoint(pygame.sprite.Sprite):
+    """This is a point on the radar that reflects the location of a space rock"""
+
+    def __init__(self, id):
+        super(RadarPoint, self).__init__()
+        self.id = id
+        # Create pygame Surface
+        self.surface = pygame.Surface((3,3))
+        self.rect = self.surface.get_rect()
+        if id == 0:
+            self.surface.fill("Green")
+        else:
+            self.surface.fill("Red")
+
+    def update(self):
+        # If the point is the player
+        if self.id == 0:
+            player_coords = radar_coord_conversion(
+                                blob.rect[0],
+                                blob.rect[1],
+                                radar_reduction,
+                                radar_left,
+                                radar_top,
+                                outer_left,
+                                outer_top
+                                )
+            self.rect[0] = player_coords[0]
+            self.rect[1] = player_coords[1]
+        else:
+            # Find the space rock with the same id and change position to match
+            for rock in rocks:
+                if self.id == rock.id:
+                    rock_coords = radar_coord_conversion(
+                                    rock.rect[0],
+                                    rock.rect[1],
+                                    radar_reduction,
+                                    radar_left,
+                                    radar_top,
+                                    outer_left,
+                                    outer_top
+                                    )
+                    self.rect[0] = rock_coords[0]
+                    self.rect[1] = rock_coords[1]
+
 class OuterBoudary():
     def __init__(self,left,right,top,bottom):
         self.left = left
@@ -183,6 +240,13 @@ class _Setup():
         sprite_group = pygame.sprite.Group()
         for i in range(num):
             sprite_group.add(self.make_random_rock(i+1))
+        return sprite_group
+
+    def make_radar_points(self, num):
+        sprite_group = pygame.sprite.Group()
+        for i in range(num):
+            point = RadarPoint(i+1)
+            sprite_group.add(point)
         return sprite_group
 
 class Game():
@@ -221,9 +285,16 @@ class Game():
                     if abs(rock_collision.velocity[0] - rock.velocity[0]) < 1 and abs(rock_collision.velocity[1] - rock.velocity[1]) < 1:
                         if rock_collision.mass > rock.mass:
                             rock_collision.mass += rock.mass
+                            for point in point_group:
+                                if point.id == rock.id:
+                                    point.kill()
                             rock.kill()
+
                         else:
                             rock.mass += rock_collision.mass
+                            for point in point_group:
+                                if point.id == rock_collision.id:
+                                    point.kill()
                             rock_collision.kill()
                         self.remaining_rocks = len(rocks.sprites())
                     else:
@@ -231,15 +302,17 @@ class Game():
                         rock_collision.velocity[1] *= collision_slow_percent
                         rock.velocity[0] *= collision_slow_percent
                         rock.velocity[1] *= collision_slow_percent
-                        print(rock_collision.id)
 
             # Check if any space rocks have collided with the player
-            collision = pygame.sprite.spritecollide(blob,rocks, True)
+            collision_rock = pygame.sprite.spritecollide(blob,rocks, True)
 
-            if collision:
+            if collision_rock:
                 # If so, then kill the space rock and add the rock's mass to the player mass
-                print( collision[0].mass )
-                blob.mass += collision[0].mass
+                print( collision_rock[0].mass )
+                blob.mass += collision_rock[0].mass
+                for point in point_group:
+                    if point.id == collision_rock[0].id:
+                        point.kill()
                 self.remaining_rocks = len(rocks.sprites())
 
             # Update player position
@@ -249,9 +322,35 @@ class Game():
             rocks.update()
             for entity in rocks:
                 self.screen.blit(entity.surface, entity.rect)
+            # Update the radar point positions
+            point_group.update()
+
+            # Draw a rectangle for the radar view
+            pygame.draw.rect(
+                self.screen,
+                (20,20,20),
+                (radar_left,
+                    radar_top,
+                    (outer_right-outer_left)*radar_reduction,
+                    (outer_bottom-outer_top)*radar_reduction)
+                    )
+            pygame.draw.rect(
+                self.screen,
+                (30,30,30),
+                (1356,
+                    840,
+                    winWidth*radar_reduction,
+                    winHeight*radar_reduction)
+            )
+
+            # Draw the radar points on the screen
+            for entity in point_group:
+                self.screen.blit(entity.surface, entity.rect)
 
             # End the game when all space rocks are gone
-            if not rocks.sprites(): self.cleanup()
+            if not rocks.sprites():
+                print("You Win!")
+                self.cleanup()
 
             # Display the current mass of the player
             mass_text_surf = self.font.render(
@@ -270,11 +369,15 @@ class Game():
 
 if __name__ == "__main__":
     # Create rock boundary
-    boundary = OuterBoudary(-winWidth,2*winWidth,-winHeight,2*winHeight)
-    # boundary = OuterBoudary(0,winWidth,0,winHeight)
+    boundary = OuterBoudary(outer_left,outer_right,outer_top,outer_bottom)
     # Create the rocks and add them to a sprite group
     setup = _Setup()
     rocks = setup.make_random_rocks(number_of_rocks)
+    # Create radar points and add them to a sprite group
+    point_group = setup.make_radar_points(number_of_rocks)
+    # Create radar point for the player
+    player_point = RadarPoint(0)
+    point_group.add(player_point)
     # Create the player sprite
     blob = Player()
     # Sprite group for all sprites
@@ -285,6 +388,17 @@ if __name__ == "__main__":
     # Sprite group just for the player
     player_group = pygame.sprite.Group()
     player_group.add(blob)
+
+    radar_origin = radar_coord_conversion(
+                                    0,
+                                    0,
+                                    radar_reduction,
+                                    radar_left,
+                                    radar_top,
+                                    outer_left,
+                                    outer_top
+                                    )
+    print(radar_origin)
 
     # Create game object and run
     game1 = Game()
